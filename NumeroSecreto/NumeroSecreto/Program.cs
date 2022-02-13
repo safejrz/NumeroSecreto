8812e44abbfe7281bd5d3b9e496ead835f9f955d
Javier Rivera <ingjrz@gmail.com>
2022-02-13 15:22:27 -0600

    Initial commit

---
+++ NumeroSecreto/NumeroSecreto/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+var min = 1;
+var max = 30;
+
+Console.WriteLine($"Hola, Adivina el numero secreto ({min}-{max}):");
+Random rnd = new Random();
+var numeroSecreto = rnd.Next(min, max);
+int x = 0;
+
+while (x != numeroSecreto)
+{
+    if (x != 0)
+    {
+        if (x > numeroSecreto)
+            Console.WriteLine("▼ --");
+        else
+            Console.WriteLine("▲ ++");
+        if (x > max)
+            Console.WriteLine($"ERROR El numero no puede ser mayor a {max}");
+        if (x < min)
+            Console.WriteLine($"ERROR El numero no puede ser menor a {min}");
+    }
+
+    var input = Console.ReadLine();
+    int.TryParse(input, out x);
+    if(x == 0)
+        Console.WriteLine($"ERROR eso no es un numero ...rata");
+}
+
+Console.WriteLine($"Felicidades! {x} es el numero secreto");
---
+++ juego.cs
@@ -0,0 +1,6 @@
+using System;
+
+Console.WriteLine("Adivina mi numero (1-20):");
+var x = Console.ReadLine();
+Console.WriteLine(x);
+
