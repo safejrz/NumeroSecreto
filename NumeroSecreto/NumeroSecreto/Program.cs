4989c0adab82c6d212721b64fbdf3e1b77182cce
diego-riveralopez <99621547+diego-riveralopez@users.noreply.github.com>
2022-03-13 11:55:57 -0600

    Added Lives and random colors
    Added Lives and random colors

--- NumeroSecreto/NumeroSecreto/Program.cs
+++ NumeroSecreto/NumeroSecreto/Program.cs
@@ -1,14 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more informationwhile
 var min = 1;
 var max = 30;
+var vidas =6;
 
-Console.WriteLine($"Hola, Adivina el numero secreto ({min}-{max}):");
 Random rnd = new Random();
+Console.BackgroundColor = (ConsoleColor)rnd.Next(1, 15);
+Console.Clear();
+Console.WriteLine($"Hola, Adivina el numero secreto ({min}-{max}):");
+
 var numeroSecreto = rnd.Next(min, max);
 int x = 0;
 
-while (x != numeroSecreto)
+while (x != numeroSecreto && vidas >= 0)
 {
+    
+    Console.ForegroundColor = (ConsoleColor)rnd.Next(1,15);
     if (x != 0)
     {
         if (x > numeroSecreto)
@@ -23,9 +29,14 @@ while (x != numeroSecreto)
 
     var input = Console.ReadLine();
     int.TryParse(input, out x);
+    vidas = vidas - 1;
+    
     if(x == 0)
         Console.WriteLine($"ERROR eso no es un numero ...rata");
     //update1
 }
 
-Console.WriteLine($"Felicidades! {x} es el numero secreto");
+if (vidas <= 0)
+    Console.WriteLine($"PERDISTE");
+else
+    Console.WriteLine($"Felicidades! {x} es el numero secreto");
